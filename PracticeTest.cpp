a0a0a3d5485454a74b45a39e4fba585d54cba743
Evan Drake <evanmdrake@icloud.com>
2022-05-02 11:41:40 -0700

    tests show only the correct impl of allnighter

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -18,3 +18,39 @@ TEST(PracticeTest, smoke_test)
 {
     ASSERT_TRUE(true);
 }
+
+TEST(PracticeTest, an_all_test)
+{
+	Practice p;
+	int sleep[7] = {0,0,0,0,0,0,0};
+	int* res = p.allnighter(sleep);
+	int* actual = &sleep[0];
+	ASSERT_EQ(actual, res);
+}
+
+TEST(PracticeTest, an_0_test)
+{
+	Practice p;
+	int sleep[7] = {7,7,7,7,7,7,7};
+	int* res = p.allnighter(sleep);
+	int* actual = nullptr;
+	ASSERT_EQ(actual, res);
+}
+
+TEST(PracticeTest, an_3_test)
+{
+	Practice p;
+	int sleep[7] = {7,7,7,0,7,7,7};
+	int* res = p.allnighter(sleep);
+	int* actual = &sleep[3];
+	ASSERT_EQ(actual, res);
+}
+
+TEST(PracticeTest, an_null_init_test)
+{
+	Practice p;
+	int sleep[7] = {7,7,7,7,7,7,7};
+	int* res = p.allnighter(sleep);
+	int* actual = &sleep[0];
+	ASSERT_NE(actual, res);
+}
